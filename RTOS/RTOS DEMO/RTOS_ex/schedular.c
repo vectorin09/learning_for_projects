0801a543066b3f5ce1a9ff27971e4dedf2c3818c
vignesh <vvvhcdsvc@gmail.com>
2025-10-31 17:31:30 +0530

    Update schedular.c
    added the priority functionality on schedular

--- RTOS/RTOS DEMO/RTOS_ex/schedular.c
+++ RTOS/RTOS DEMO/RTOS_ex/schedular.c
@@ -3,23 +3,41 @@
 #include"schedular.h"
 Task task[max_task];
 static int task_count=0;
+void schedular_arrange(void);
 
 void schedular_init(void)
 {
 	task_count=0;
 }
-void schedular_add_task(void(*task_argument)(void),unsigned int interval)
+void schedular_add_task(void(*task_argument)(void),unsigned int interval,int priority)
 {
 	if(task_count<max_task)
 	{
 		task[task_count].task_function=task_argument;
 		task[task_count].interval=interval;
 		task[task_count].last_run=0;
+		task[task_count].priority=priority;
 		task_count++;
 	}
 	
 
 }
+void schedular_arrange(void)
+{
+	for(int i=0;i<task_count;i++)
+	{
+		for(int j=0;j<task_count-i-1;j++)
+		{
+			if(task[j].priority > task[j+1].priority)
+			{
+				 Task temp= task[j];
+				task[j]=task[j+1];
+				task[j+1]=temp;
+			}
+		}
+	}
+}
+
 void schedular_run(void)
 {
 	 unsigned int current_time=milli();
