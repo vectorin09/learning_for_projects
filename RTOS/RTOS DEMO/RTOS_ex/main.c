153459af5ac42367aa7b91dae06c2a1985fb566c
vignesh <vvvhcdsvc@gmail.com>
2025-10-31 17:29:13 +0530

    Update main.c
    adding the priority functionality

--- RTOS/RTOS DEMO/RTOS_ex/main.c
+++ RTOS/RTOS DEMO/RTOS_ex/main.c
@@ -6,15 +6,16 @@
 
 int main()
 {
-	printf("\n=====mini rtos schedular======\n");
-	schedular_init();
-	timer_init();
+        printf("\n=====mini rtos schedular======\n");
+        schedular_init();
+        timer_init();
 
-	schedular_add_task(task_led_blink,1000);
-	schedular_add_task(task_print_time,2000);
-	schedular_add_task(task_sensor_read,3000);
-	while(1)
-	{
-		schedular_run();
-	}
+        schedular_add_task(task_led_blink,1000,0);
+        schedular_add_task(task_print_time,2000,1);
+        schedular_add_task(task_sensor_read,3000,2);
+        while(1)
+        {
+                schedular_run();
+        }
 }
+~
